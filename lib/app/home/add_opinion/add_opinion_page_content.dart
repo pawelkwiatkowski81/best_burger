4a2eee8b5503bb3ced4129e4fa100b0b1da0f412
Pawel Kwiatkowski <pawelkwiatkowski81@gmail.com>
2023-04-06 23:21:22 +0200

    Implement Add Restaurants

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     super.key,
   });
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+var restaurantName = '';
+var burgerName = '';
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('T W O'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(hintText: 'Add Place/Restaurant'),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(hintText: 'Add Burger'),
+            onChanged: (newValue) {
+              setState(() {
+                burgerName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('Restaurants').add({
+                'name': restaurantName,
+                'burger': burgerName,
+                'rating': 5.0,
+              });
+            },
+            child: const Text('Add'),
+          )
+        ],
+      ),
     );
   }
 }
